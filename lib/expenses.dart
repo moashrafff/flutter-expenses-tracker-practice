67dfe034b474d6a614097f9ca5cae27f4824f552
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2024-11-15 15:16:34 +0200

    creating dummy values

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 
+import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -11,6 +12,20 @@ class Expenses extends StatefulWidget {
 }
 
 class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
