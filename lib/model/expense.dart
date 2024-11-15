bee8cc785c429d915fc107974a2d9c8e33a54de9
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2024-11-15 19:08:26 +0200

    implementation of expenses list and items and using intl and uuid

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,10 +1,20 @@
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+final formatter = DateFormat.yMd();
+
 enum Category { food, leisure, work, travel }
 
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
 
@@ -20,4 +30,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
