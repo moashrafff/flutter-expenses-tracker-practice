154631e8908cb97a1e6e3743471e51e7d8dc894d
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2024-11-22 18:43:59 +0200

    widgets theme

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -15,14 +15,18 @@ class ExpenseItem extends StatelessWidget {
           vertical: 16,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
             const SizedBox(height: 4),
             Row(
               children: [
                 Text('\$${expense.amount.toStringAsFixed(2)}'),
                 const Spacer(),
-                Row(
+                Row(  
                   children: [
                     Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8),
@@ -37,4 +41,3 @@ class ExpenseItem extends StatelessWidget {
     );
   }
 }
-
